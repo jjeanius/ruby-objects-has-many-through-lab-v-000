c11831209b415f7d6cb6972744fd4bec25607fc1
Judy McGoldrick <jjeanius@verizon.net>
2018-01-25 02:35:19 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Appointment
+
+  attr_accessor :doctor, :patient, :name
+
+  def initialize(doctor, date)
+    @doctor = doctor
+    @date = date
+  end
+
+  def patient
+    @patient
+  end
+
+  def doctor
+    appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+end
+
+    #@name = name
+    #self.patient = patient
+    #@date = date
+    #@doctor = doctor
+    #@appointment = appointment
+    #
+    #patient.add_appointment(self)
+    # appointment = appointment.new
