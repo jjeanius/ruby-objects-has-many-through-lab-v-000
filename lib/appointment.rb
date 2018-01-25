c93d5f68ddce3d7e13476161503d75d0f15b6632
Judy McGoldrick <jjeanius@verizon.net>
2018-01-25 02:55:17 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Appointment
 
-  attr_accessor :doctor, :patient, :name, appointment
+  attr_accessor :doctor, :patient, :name, :appointment
 
   def initialize(doctor, date)
     @doctor = doctor
